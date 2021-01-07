050b2957e4020a3fee3f8969cc88e4819bbbed9f
The-Box-Lord <56173725+The-Box-Lord@users.noreply.github.com>
2021-01-07 16:30:24 -0600

    add basic files

--- gl_main.c
+++ gl_main.c
@@ -49,11 +49,10 @@ static void gl_fini(Int exitcode)
 
 static void gl_pre_clo_init(void)
 {
-   VG_(details_name)            ("Nulgrind");
+   VG_(details_name)            ("Gleipnir");
    VG_(details_version)         (NULL);
-   VG_(details_description)     ("the minimal Valgrind tool");
-   VG_(details_copyright_author)(
-      "Copyright (C) 2002-2017, and GNU GPL'd, by Nicholas Nethercote.");
+   VG_(details_description)     ("a Valgrind tool");
+   VG_(details_copyright_author)("Copyright (C) 2021");
    VG_(details_bug_reports_to)  (VG_BUGS_TO);
 
    VG_(details_avg_translation_sizeB) ( 275 );
